371fd2eabd46a0a46bbcd9a6e0e3b4b315e3dfee
john katua <johnkatua99@gmail.com>
2020-07-10 02:41:32 +0300

    imported SVG into our project and we also styled our header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,10 +2,43 @@ import React from 'react'
 
 import './header.scss';
 
+import logo from '../../images/logo.png';
+import user from '../../images/user.jpg';
+import {ReactComponent as Search} from '../../images/SVG/magnifying-glass.svg';
+import {ReactComponent as Bookmark} from '../../images/SVG/bookmark.svg';
+import {ReactComponent as Chat} from '../../images/SVG/chat.svg';
+
 const Header = () => {
   return (
     <div className='header'>
-      Header part
+      <img src={logo} alt = "logo" className='logo'/>
+
+      <form action='#' className='search'>
+        <input type='text' className='search__input' placeholder='search hotels' />
+        <button className='search__button'>
+          <svg className='search__icon'>
+            <Search />
+          </svg>
+        </button>
+      </form>
+      <nav className='user-nav'>
+        <div className='user-nav__icon-box'>
+          <svg className='user-nav__icon'>
+             <Bookmark />
+          </svg>
+          <span className='user-nav__notification'>45</span>
+        </div>
+        <div className='user-nav__icon-box'>
+          <svg className='user-nav__icon'>
+             <Chat />
+          </svg>
+          <span className='user-nav__notification'>21</span>
+        </div>
+        <div className="user-nav__user">
+           <img src={user} alt = 'user' className='user-nav__user-photo' />
+           <span className="user-nav__user-name">katua</span>
+        </div>
+      </nav>
     </div>
   )
 };
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './homepage.scss';
-import Container from '../container/container';
+import Container from '../../components/container/container';
 
 const HomePage = () => {
   return (
